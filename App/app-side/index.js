2ef885fa70520138b235814d2278867c43392d44
Noxde <mateo.tesorieri@hotmail.com>
2024-05-03 18:37:01 -0300

    fetchData ahora son 2 funciones, una para cada parada

--- App/app-side/index.js
+++ App/app-side/index.js
@@ -2,12 +2,33 @@ import { MessageBuilder } from "../shared/message";
 
 const messageBuilder = new MessageBuilder();
 
-async function fetchData(ctx) {
+async function fetchCasa(ctx) {
   try {
-    const resBody = await fetch("http://127.0.0.1:3000")
+    const resBody = await fetch("http://127.0.0.1:3000/casa")
       .then((response) => response.json())
       .then((data) => data)
-      .catch((err) => console.log(err));
+      .catch((err) => ({
+        code: 0,
+      }));
+
+    ctx.response({
+      data: { result: resBody },
+    });
+  } catch (error) {
+    ctx.response({
+      data: { result: "ERROR" },
+    });
+  }
+}
+
+async function fetchFacultad(ctx) {
+  try {
+    const resBody = await fetch("http://127.0.0.1:3000/facultad")
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((err) => ({
+        code: 0,
+      }));
 
     ctx.response({
       data: { result: resBody },
